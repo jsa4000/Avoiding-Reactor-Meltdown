7b6b3af7a7291b0cea32215959e734ad9ba917cf
Javier Santos Andrés <jsantosa@MacBook-Pro.local>
2022-11-02 23:23:48 +0100

    Added next Loom Project

---
+++ src/test/java/com/example/demo/threading/LoomProjectIT.java
@@ -0,0 +1,25 @@
+package com.example.demo.threading;
+
+import org.junit.jupiter.api.Test;
+
+public class LoomProjectIT {
+
+    /**
+     *
+     * Project Loom is an attempt by the OpenJDK community to introduce a lightweight
+     * concurrency construct to Java. The prototypes for Loom so far have introduced a
+     * change in the JVM as well as the Java library.
+     *
+     * Although there is no scheduled release for Loom yet, we can access the recent
+     * prototypes on Project Loom's wiki.
+     *
+     * Before we discuss the various concepts of Loom, let's discuss the current concurrency
+     * model in Java.
+     */
+
+    @Test
+    void simpleLoomTest() {
+
+    }
+
+}
